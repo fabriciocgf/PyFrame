1f88bd8b07d7554ab505b2376222a0be3220c398
FABRICIO CESAR GOMES FILHO <fabriciocgf@gmail.com>
2020-12-13 02:09:59 -0300

    first commit

---
+++ PyFrame.py
@@ -0,0 +1,70 @@
+import pygame, random
+from pygame.locals import *
+import os
+ddd
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 240
+FADE_SPEED = 10
+
+class Photo(pygame.sprite.Sprite):
+
+    def __init__(self, photopath):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(photopath).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        self.alpha = self.image.get_alpha()
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = 0
+        self.rect[1] = 0
+
+        print(self.alpha)
+
+    def update(self):
+        self.alpha -= FADE_SPEED
+        self.image.set_alpha(self.alpha)
+        print(self.alpha)
+
+
+def is_off_screen(sprite):
+    return sprite.alpha <= 0
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    photo_group = pygame.sprite.Group()
+    assetlist = ['assets/noise_1.jpg','assets/noise_2.jpg','assets/noise_3.jpg']
+    for i in range(2):
+        photo = Photo(assetlist[i])
+        photo_group.add(photo)
+
+    clock = pygame.time.Clock()
+    i=0
+    while True:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    photo.update()
+
+
+        if is_off_screen(photo_group.sprites()[0]):
+            photo_group.remove(photo_group.sprites()[0])
+            new_photo = Photo(assetlist[i])
+            photo_group.add(new_photo)
+            i += 1
+            if i==3:
+                i=1
+
+        print(photo_group.sprites())
+        photo_group.draw(screen)
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
