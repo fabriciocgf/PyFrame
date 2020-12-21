9020207d6886250dd8777d8167b2ab4d5625a78b
fabriciocgf <fabriciocgf@gmail.com>
2020-12-21 19:52:43 -0300

    update animation

--- src/PyFrame.py
+++ src/PyFrame.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 import os
 import stat
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 720
 FADE_SPEED = 10
 duration_millis = 1 * 1000
 i=2
